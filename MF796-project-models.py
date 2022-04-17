ba12a4907401ddecf03eef5f56629e64ecdb970b
tkuntz20 <thomaskuntz20@gmail.com>
2022-04-17 12:57:35 -0400

    Create MF796-project-models.py

---
+++ MF796-project-models.py
@@ -0,0 +1,81 @@
+""""
+Group term-project for MF 796 Computational Methods
+@Authers: Kibae Kim, Tiaga Schwarz, Karen Mu, Thomas Kuntz
+"""
+
+import pandas as pd
+import numpy as np
+from math import *
+import QuantLib as ql
+import scipy as sp
+import scipy.stats as si
+import statsmodels.api as sm
+import seaborn as sns
+import sympy as sy
+from scipy.optimize import newton
+from tabulate import tabulate
+from pandas_datareader import data
+import matplotlib.pyplot as plt
+from datetime import datetime
+import calendar
+
+
+class Base:
+
+    def __init__(self, one):
+        self.one = one
+
+    def __repr__(self):
+
+        return f'nothing'
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+if __name__ == '__main__':      # ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+
+
+
+
+
+
+
+
+
+
+
+
+
